303cd507e05fbbaf5005c3270d43d3174673e439
rukaboy <wimalarathnarukshan2@gmail.com>
2025-11-18 15:31:33 +0530

    add head

---
+++ resources/views/component/head.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
+</head>
+<body>
