7f5405f8a2a1245e457ae85906b0628b269d1350
rukaboy <wimalarathnarukshan2@gmail.com>
2025-11-18 15:34:54 +0530

    create admin controller
    added admin controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function dashboard(): Factory|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('dashboard');
+
+    }
+    public function about()
+    {
+        return view('about');
+    }
+}
