b267016739d6d8c26c881d2ae760ad4a1c2527a8
rukaboy <wimalarathnarukshan2@gmail.com>
2025-11-18 15:28:04 +0530

    added about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@include('component.head')
+@include('component.navbar')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2>About Us</h2>
+        </div>
+    </div>
+</div>
+@include('component.script')
