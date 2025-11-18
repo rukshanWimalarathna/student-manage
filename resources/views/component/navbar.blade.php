b0cb3174059b96302467c4696846b780e3b72811
rukaboy <wimalarathnarukshan2@gmail.com>
2025-11-18 15:35:23 +0530

    add nav bar
    add nav bar for ui

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-dark-subtle">
+    <a class="navbar-brand" href="#">Student Management</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{route('admin.dashboard')}}">Home </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('admin.about')}}">About US</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
