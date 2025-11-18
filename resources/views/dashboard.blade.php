21cae00ddb079a01bf1dbc90f3c2809aa33f9f4a
rukaboy <wimalarathnarukshan2@gmail.com>
2025-11-18 15:31:01 +0530

    add dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@include('component.head')
+@include('component.navbar')
+<div class="container">
+    <div class="row">
+      <div class="col-6">
+          <h2>CodeXpress</h2>
+      </div>
+    </div>
+</div>
+
+@include('component.script')
+
