4909aa4fbaf045d2573f277d9cc1f11cb96e3b0e
rukaboy <wimalarathnarukshan2@gmail.com>
2025-11-20 00:18:40 +0530

    summry
    work with form, blade, navbar and dashboard

--- resources/views/component/head.blade.php
+++ resources/views/component/head.blade.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
+    <title>
+        @stack('title')
+
+    </title>
     <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
+    @stack('css')
 </head>
 <body>
--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -1,15 +1,17 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-dark-subtle">
-    <a class="navbar-brand" href="#">Student Management</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-success">
+    <a class="navbar-brand" href="#">
+        @stack('nav-brand')
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="{{route('admin.dashboard')}}">Home </a>
+                <a class="nav-link" href="">Home </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{route('admin.about')}}">About US</a>
+                <a class="nav-link" href="">About US</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Contact Us</a>
