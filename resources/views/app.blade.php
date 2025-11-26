729541a0218068da02db48c16b7f4af407b3268d
rukaboy <wimalarathnarukshan2@gmail.com>
2025-11-26 16:08:43 +0530

    update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,13 @@
 <!--begin::App Wrapper-->
 <div class="app-wrapper">
     <!--begin::Header-->
-
     @include('component.navbar')
     <!--end::Header-->
+
     <!--begin::Sidebar-->
     @include('component.sidebar')
     <!--end::Sidebar-->
+
     <!--begin::App Main-->
     <main class="app-main">
         <!--begin::App Content Header-->
@@ -16,7 +17,7 @@
             <!--begin::Container-->
             <div class="container-fluid">
 
-            @yield('content')
+                @yield('content')
 
             </div>
             <!--end::Container-->
@@ -24,14 +25,18 @@
         <!--end::App Content-->
     </main>
     <!--end::App Main-->
+
     <!--begin::Footer-->
     @include('component.footer')
     <!--end::Footer-->
 </div>
 <!--end::App Wrapper-->
 
-<!-- All scripts remain exactly the same -->
-<!--begin::Script-->
-<!--begin::Third Party Plugin(OverlayScrollbars)-->
+<!-- Scripts -->
 @include('component.script')
-<!--end::Script-->
+
+<! VERY IMPORTANT - Loads page-specific scripts -->
+@stack('script')
+
+</body>
+</html>
